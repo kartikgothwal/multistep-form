8fdc9f3219bf6eca79546dd1c525029f97f41710
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-07-07 23:26:12 +0530

    added the zod schema

--- src/app/constant/index.ts
+++ src/app/constant/index.ts
@@ -1,4 +1,5 @@
 import z from "zod";
+import { tr } from "zod/v4/locales";
 
 export const profileSchema = z.object({
   name: z
@@ -14,4 +15,31 @@ export const profileSchema = z.object({
     .string({ required_error: "Bio is required" })
     .min(10, { message: "Bio must be at least 10 characters long" })
     .max(200, { message: "Bio must be at most 200 characters long" }),
+});
+
+export const interestsSchema = z.object({
+  hoobies: z
+    .array(z.string().min(1, { message: "At least one hobby is required" }))
+    .min(1, { message: "At least one hobby is required" }),
+  skills: z
+    .array(z.string().min(1, { message: "At least one skill is required" }))
+    .min(1, { message: "At least one skill is required" }),
+  walk: z
+    .string({ required_error: "Walk is required" })
+    .refine((value) => value.trim() !== "", {
+      message: "Walk is required",
+    })
+    .refine((value) => (value === "yes" ? true : false), {
+      message: "Walk must be 'yes' or 'no'",
+    }),
+});
+export const settingsSchema = z.object({
+  notifications: z.boolean().default(false),
+  theme: z.enum(["light", "dark"]).default("light"),
+});
+
+export const termsSchema = z.object({
+  acceptTerms: z.boolean().refine((value) => value === true, {
+    message: "You must accept the terms and conditions",
+  }),
 });
