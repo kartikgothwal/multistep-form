5c91a80caa465fdb6abc4921ae91790d8f8f4103
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-07-08 23:40:48 +0530

    added the other schemas as well

--- src/app/constant/index.ts
+++ src/app/constant/index.ts
@@ -1,15 +1,3 @@
-import z from "zod";
- 
+  
 
 
-
-export const settingsSchema = z.object({
-  notifications: z.boolean().default(false),
-  theme: z.enum(["light", "dark"]).default("light"),
-});
-
-export const termsSchema = z.object({
-  acceptTerms: z.boolean().refine((value) => value === true, {
-    message: "You must accept the terms and conditions",
-  }),
-});
--- src/app/zod.ts
+++ src/app/zod.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export const profileSchema = z.object({
   name: z
     .string({ required_error: "Name is required" })
@@ -28,4 +30,15 @@ export const interestsSchema = z.object({
     .refine((value) => (value === "yes" ? true : false), {
       message: "Walk must be 'yes' or 'no'",
     }),
+});
+
+export const settingsSchema = z.object({
+  notifications: z.boolean().default(false),
+  theme: z.enum(["light", "dark"]).default("light"),
+});
+
+export const termsSchema = z.object({
+  acceptTerms: z.boolean().refine((value) => value === true, {
+    message: "You must accept the terms and conditions",
+  }),
 });
