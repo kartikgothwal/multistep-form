2b35f39c684f54995a3d14db431ba48b231a3cfd
Kartik Gothwal <kartikgothwal28@gmail.com>
2025-07-08 23:40:09 +0530

    adding the schema in the seperate file

---
+++ src/app/zod.ts
@@ -0,0 +1,31 @@
+export const profileSchema = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email({ message: "Invalid email address" })
+    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: "Email must be a valid email address",
+    }),
+  bio: z
+    .string({ required_error: "Bio is required" })
+    .min(10, { message: "Bio must be at least 10 characters long" })
+    .max(200, { message: "Bio must be at most 200 characters long" }),
+});
+export const interestsSchema = z.object({
+  hoobies: z
+    .array(z.string().min(1, { message: "At least one hobby is required" }))
+    .min(1, { message: "At least one hobby is required" }),
+  skills: z
+    .array(z.string().min(1, { message: "At least one skill is required" }))
+    .min(1, { message: "At least one skill is required" }),
+  walk: z
+    .string({ required_error: "Walk is required" })
+    .refine((value) => value.trim() !== "", {
+      message: "Walk is required",
+    })
+    .refine((value) => (value === "yes" ? true : false), {
+      message: "Walk must be 'yes' or 'no'",
+    }),
+});
